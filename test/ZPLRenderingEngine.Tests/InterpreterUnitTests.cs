9cc1fc312c7e0ff4f84dcd5b9b043725deae1316
SilentBlue <SilentBlueD666@users.noreply.github.com>
2019-08-05 23:02:25 +0100

    Added check for command code in tests.

--- test/ZPLRenderingEngine.Tests/InterpreterUnitTests.cs
+++ test/ZPLRenderingEngine.Tests/InterpreterUnitTests.cs
@@ -10,22 +10,24 @@ namespace ZplRenderingEngine.Tests
         public void Ensure_First_Command_Is_XA_Label_Start_Command()
         {
             string zpl = "^XA^FO50,50^ADN,36,20^FDSample^FS^XZ";
-            var zplDocument = ZplDocument.CreateAndBuild(zpl);
 
+            var zplDocument = ZplDocument.CreateAndBuild(zpl);
             var startCommand = zplDocument.ZplCommands.FirstOrDefault() as XA_ZplCommand;
 
             Assert.True(startCommand is XA_ZplCommand);
+            Assert.Equal("^XA", startCommand.Code);
         }
 
         [Fact]
         public void Ensure_Last_Command_Is_XZ_Label_End_Command()
         {
             string zpl = "^XA^FO50,50^ADN,36,20^FDSample^FS^XZ";
-            var zplDocument = ZplDocument.CreateAndBuild(zpl);
 
+            var zplDocument = ZplDocument.CreateAndBuild(zpl);
             var lastCommand = zplDocument.ZplCommands.LastOrDefault() as XZ_ZplCommand;
 
             Assert.True(lastCommand is XZ_ZplCommand);
+            Assert.Equal("^XZ", lastCommand.Code);
         }
     }
 }
